c468a03e7ead2f20e7aee177041837b7d168c738
Maciej Litwiniuk <maciej@galdomedia.pl>
2009-10-12 00:29:36 +0200

    temporary file must have .html extension

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -21,7 +21,7 @@ class WickedPdf
     path << ' -q'
 
     logger.info "\n\n-- wkhtmltopdf command --"
-    tmp_file = Tempfile.new('wicked_pdf', 'tmp')
+    tmp_file = Tempfile.new(['wicked_pdf', '.html'], 'tmp')
     tmp_file.write(string)
     tmp_file.close
     path = path + ' ' + tmp_file.path + ' -'
