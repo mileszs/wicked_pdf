af18210f5b301685c81cf25c28d9d08cf93a998e
James <james@ubun.(none)>
2009-11-07 00:58:42 +1100

    First take on using streams rather than temp files to avoid ruby 1.86 temp file extension issues

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -3,7 +3,7 @@
 
 require 'logger'
 require 'digest/md5'
-require 'tempfile'
+require 'open3'
 
 class WickedPdf
   attr_accessor :exe_path, :log_file, :logger
@@ -16,21 +16,11 @@ class WickedPdf
   end
 
   def pdf_from_string(string)
-    path = @exe_path
-    # Don't output errors to standard out
-    path << ' -q'
-
-    logger.info "\n\n-- wkhtmltopdf command --"
-    tmp_file = Tempfile.new(['wicked_pdf', '.html'], 'tmp')
-    tmp_file.write(string)
-    tmp_file.close
-    path = path + ' ' + tmp_file.path + ' -'
-
-    logger.info path
-    logger.info ''
-
-    pdf = `#{path}`
-    tmp_file.unlink
-    pdf
+    command_for_stdin_stdout = @exe_path.strip + " - - -q" # -q for no errors on stdout
+    Open3.popen3(command_for_stdin_stdout) do |stdin, stdout, stderr|
+      stdin.write(string)
+      stdin.close
+      stdout.read
+    end
   end
 end
