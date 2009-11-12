e9a7d90b9a5555e19ae9e62f5317f90823a35c77
James Crisp <james@Macel.local>
2009-11-12 14:40:28 +1100

    Adding basic error handling and spec integration tests

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -6,21 +6,20 @@ require 'digest/md5'
 require 'open3'
 
 class WickedPdf
-  attr_accessor :exe_path, :log_file, :logger
 
-  def initialize(wkhtmltopdf_binary = nil)
-    @exe_path = wkhtmltopdf_binary 
+  def initialize(wkhtmltopdf_binary_path = nil)
+    @exe_path = wkhtmltopdf_binary_path 
     @exe_path ||= `which wkhtmltopdf`.chomp
-    @log_file = "#{RAILS_ROOT}/log/wkhtmltopdf.log"
-    @logger   = RAILS_DEFAULT_LOGGER
   end
 
   def pdf_from_string(string)
-    command_for_stdin_stdout = @exe_path.strip + " - - -q" # -q for no errors on stdout
+    command_for_stdin_stdout = @exe_path + " - - -q" # -q for no errors on stdout
     Open3.popen3(command_for_stdin_stdout) do |stdin, stdout, stderr|
       stdin.write(string)
       stdin.close
-      stdout.read
+      pdf = stdout.read
+      raise "PDF could not be generated!\n#{stderr.read}" if pdf.length == 0
+      pdf
     end
   end
 end
