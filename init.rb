c4ea96ca9224144afa1a22b7b9939895d7b6ab8a
lleir <memtest@gmail.com>
2009-11-16 15:26:59 +0100

    allow customized headers and footers

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 require 'wicked_pdf'
 require 'pdf_helper'
+require 'tempfile'
  
 Mime::Type.register 'application/pdf', :pdf
- 
+
 ActionController::Base.send(:include, PdfHelper)
--- lib/pdf_helper.rb
+++ lib/pdf_helper.rb
@@ -17,22 +17,44 @@ module PdfHelper
   end
 
   private
+    def make_pdf(options = {})
+      options[:wkhtmltopdf] ||= nil
+      options[:layout] ||= false
+      options[:template] ||= File.join(controller_path, action_name)
 
-  def make_pdf(options = {})
-    options[:wkhtmltopdf] ||= nil
-    options[:layout] ||= false
-    options[:template] ||= File.join(controller_path, action_name)
+      html_string = render_to_string(:template => options[:template], :layout => options[:layout])
+      w = WickedPdf.new(options[:wkhtmltopdf])
+      w.pdf_from_string(html_string, parse_header_footer(:header => options[:header], :footer => options[:footer]))
+    end
 
-    html_string = render_to_string(:template => options[:template], :layout => options[:layout])
-    w = WickedPdf.new(options[:wkhtmltopdf])
-    w.pdf_from_string(html_string)
-  end
+    def make_and_send_pdf(pdf_name, options = {})
+      send_data(
+        make_pdf(options),
+        :filename => pdf_name + '.pdf',
+        :type => 'application/pdf'
+      )
+    end
 
-  def make_and_send_pdf(pdf_name, options = {})
-    send_data(
-      make_pdf(options),
-      :filename => pdf_name + '.pdf',
-      :type => 'application/pdf'
-    )
-  end
+    def parse_header_footer opts
+      r=""
+      [:header, :footer].collect do |hf| 
+        unless opts[hf].blank?
+          r += [:center, :font_name, :font_size, :left, :right, :spacing].collect do |o|
+            "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} '#{opts[hf][o]}'" unless opts[hf][o].blank?
+          end.join(' ')
+          r += "--#{hf.to_s}-line " unless opts[hf][:line].blank?
+          unless opts[hf][:html].blank?
+            r += "--#{hf.to_s}-html '#{opts[hf][:html][:url]}' " unless opts[hf][:html][:url].blank?
+            unless opts[hf][:html][:template].blank?
+              Tempfile.open("pdf.html") do |f|
+                p f.path
+                f << render_to_string(opts[hf][:html][:template])
+                r += "--#{hf.to_s}-html 'file://#{f.path}' "
+              end
+            end
+          end
+        end
+      end
+      r
+    end
 end
