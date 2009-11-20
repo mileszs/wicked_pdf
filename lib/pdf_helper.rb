aced8ab1b8e14eca13737a97300784d47f1e2b74
lleir <memtest@gmail.com>
2009-11-20 12:26:12 +0100

    optimizing code

--- lib/pdf_helper.rb
+++ lib/pdf_helper.rb
@@ -31,40 +31,42 @@ module PdfHelper
       if options[:show_as_html]
         render :text => render_to_string(:template => options[:template], :layout => options[:layout])
       else
-        send_data(
-          make_pdf(options),
-          :filename => pdf_name + '.pdf',
-          :type => 'application/pdf'
-        )
+        send_data(make_pdf(options), :filename => pdf_name + '.pdf', :type => 'application/pdf')
       end
     end
 
     def parse_options opts
-      "#{parse_header_footer(:header => opts.delete(:header), :footer => opts.delete(:footer), :layout => opts[:layout])} \
-       #{parse_toc(opts.delete(:toc))} \
-       #{parse_outline(opts.delete(:outline))} \
-       #{parse_margins(opts.delete(:margin))} \
-       #{parse_others(opts)}"
+      s = "#{parse_header_footer(:header => opts.delete(:header), :footer => opts.delete(:footer), :layout => opts[:layout])} "
+      s += "#{parse_toc(opts.delete(:toc))} #{parse_outline(opts.delete(:outline))} "
+      s + "#{parse_margins(opts.delete(:margin))} #{parse_others(opts)} "
+    end
+
+    def make_option name, value, type=:string
+      "--#{name.gsub('_', '-')} " + case type
+        when :boolean: ""
+        when :numeric: value.to_s
+        else "'#{value}'"
+      end + " "
+    end
+
+    def make_options opts, names, prefix="", type=:string
+      names.collect {|o| make_option("#{prefix.blank? ? "" : prefix + "-"}#{o.to_s}", opts[o], type) unless opts[o].blank?}.join
     end
 
     def parse_header_footer opts
       r=""
       [:header, :footer].collect do |hf| 
         unless opts[hf].blank?
-          r += [:center, :font_name, :left, :right].collect do |o|
-            "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} '#{opts[hf][o]}' " unless opts[hf][o].blank?
-          end.join
-          r += [:font_size, :spacing].collect do |o|
-            "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} #{opts[hf][o]} " unless opts[hf][o].blank?
-          end.join
-          r += "--#{hf.to_s}-line " unless opts[hf][:line].blank?
-          unless opts[hf][:html].blank?
-            r += "--#{hf.to_s}-html '#{opts[hf][:html][:url]}' " unless opts[hf][:html][:url].blank?
-            unless opts[hf][:html][:template].blank?
-              Tempfile.open("pdf.html") do |f|
-                p f.path
-                f << render_to_string(:template => opts[hf][:html][:template], :layout => opts[:layout])
-                r += "--#{hf.to_s}-html 'file://#{f.path}' "
+          opt_hf = opts[hf]
+          r += make_options(opt_hf, [:center, :font_name, :left, :right], "#{hf.to_s}")
+          r += make_options(opt_hf, [:font_size, :spacing], "#{hf.to_s}", :numeric)
+          r += make_options(opt_hf, [:line], "#{hf.to_s}", :boolean)
+          unless opt_hf[:html].blank?
+            r += make_option("#{hf.to_s}-html", opt_hf[:html][:url]) unless opt_hf[:html][:url].blank?
+            unless opt_hf[:html][:template].blank?
+              Tempfile.open("wicked_pdf.html") do |f|
+                f << render_to_string(:template => opt_hf[:html][:template], :layout => opts[:layout])
+                r += make_option("#{hf.to_s}-html", "file://#{f.path}")
               end
             end
           end
@@ -75,53 +77,28 @@ module PdfHelper
 
     def parse_toc opts
       unless opts.blank?
-        r=""
-        r += [:font_name, :header_text].collect do |o|
-          "--toc-#{o.to_s.gsub('_', '-')} '#{opts[o]}' " unless opts[o].blank?
-        end.join
-        r += [:depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size, :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation, :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation].collect do |o|
-          "--toc-#{o.to_s.gsub('_', '-')} #{opts[o]} " unless opts[o].blank?
-        end.join
-        r += [:no_dots, :disable_links, :disable_back_links].collect do |o|
-          "--toc-#{o.to_s.gsub('_', '-')} " unless opts[o].blank?
-        end.join
-        r
+        r = make_options(opts, [:font_name, :header_text], "toc")
+        r += make_options(opts, [:depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size, :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation, :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation], "toc", :numeric)
+        r + make_options(opts, [:no_dots, :disable_links, :disable_back_links], "toc", :boolean)
       end
     end
 
     def parse_outline opts
       unless opts.blank?
-        r=""
-        r += "--outline " unless opts[:outline].blank?
-        r += "--outline-depth '#{opts[:outline_depth]}' " unless opts[:outline_depth].blank?
-        r
+        r = make_options(opts, [:outline], "", :boolean)
+        r + make_options(opts, [:outline_depth])
       end
     end
 
     def parse_margins opts
-      unless opts.blank? 
-        r=""
-        r += "--margin-top #{opts[:top]} " unless opts[:top].blank?
-        r += "--margin-bottom #{opts[:bottom]} " unless opts[:bottom].blank?
-        r += "--margin-left #{opts[:left]} " unless opts[:left].blank?
-        r += "--margin-right #{opts[:right]} " unless opts[:right].blank?
-        r
-      end
+      make_options(opts, [:top, :bottom, :lef, :right], "margin")
     end
     
     def parse_others opts
       unless opts.blank? 
-        r=""
-        r += [:orientation, :page_size, :proxy, :username, :password, :cover, :dpi, :encoding, :user_style_sheet].collect do |o|
-          "--#{o.to_s.gsub('_', '-')} '#{opts[o]}' " unless opts[o].blank?
-        end.join
-        r += [:redirect_delay, :zoom, :page_offset].collect do |o|
-          "--#{o.to_s.gsub('_', '-')} #{opts[o]} " unless opts[o].blank?
-        end.join
-        r += [:book, :default_header, :toc, :disable_javascript, :greyscale, :lowquality, :enable_plugins, :disable_internal_links, :disable_external_links, :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background].collect do |o|
-          "--#{o.to_s.gsub('_', '-')} " unless opts[o].blank?
-        end.join
-        r
+        r = make_options(opts, [:orientation, :page_size, :proxy, :username, :password, :cover, :dpi, :encoding, :user_style_sheet])
+        r += make_options(opts, [:redirect_delay, :zoom, :page_offset], "", :numeric)
+        r + make_options(opts, [:book, :default_header, :toc, :disable_javascript, :greyscale, :lowquality, :enable_plugins, :disable_internal_links, :disable_external_links, :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background], "", :boolean)
       end
     end
 end
