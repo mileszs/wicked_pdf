8c80b56094ae00d4c5f30b70fb3c0519fd979a8c
Reid Beels <mail@reidbeels.com>
2010-05-11 12:22:25 -0700

    Refactored WickedPdf and WickedPdfTest for readability. No functional changes.

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -28,9 +28,16 @@ class WickedPdf
   end
 
   private
-  def parse_options opts
-    "#{parse_header_footer(:header => opts.delete(:header), :footer => opts.delete(:footer), :layout => opts[:layout])} " + \
-    "#{parse_toc(opts.delete(:toc))} #{parse_outline(opts.delete(:outline))} #{parse_margins(opts.delete(:margin))} #{parse_others(opts)} "
+  def parse_options(options)
+    [
+      parse_header_footer(:header => options.delete(:header),
+                          :footer => options.delete(:footer),
+                          :layout => options[:layout]),
+      parse_toc(options.delete(:toc)),
+      parse_outline(options.delete(:outline)),
+      parse_margins(options.delete(:margin)),
+      parse_others(options),
+    ].join(' ')
   end
 
   def make_option name, value, type=:string
@@ -41,15 +48,15 @@ class WickedPdf
     end + " "
   end
 
-  def make_options opts, names, prefix="", type=:string
-    names.collect {|o| make_option("#{prefix.blank? ? "" : prefix + "-"}#{o.to_s}", opts[o], type) unless opts[o].blank?}.join
+  def make_options options, names, prefix="", type=:string
+    names.collect {|o| make_option("#{prefix.blank? ? "" : prefix + "-"}#{o.to_s}", options[o], type) unless options[o].blank?}.join
   end
 
-  def parse_header_footer opts
+  def parse_header_footer options
     r=""
     [:header, :footer].collect do |hf|
-      unless opts[hf].blank?
-        opt_hf = opts[hf]
+      unless options[hf].blank?
+        opt_hf = options[hf]
         r += make_options(opt_hf, [:center, :font_name, :left, :right], "#{hf.to_s}")
         r += make_options(opt_hf, [:font_size, :spacing], "#{hf.to_s}", :numeric)
         r += make_options(opt_hf, [:line], "#{hf.to_s}", :boolean)
@@ -57,34 +64,72 @@ class WickedPdf
           r += make_option("#{hf.to_s}-html", opt_hf[:html][:url]) unless opt_hf[:html][:url].blank?
         end
       end
-    end unless opts.blank?
+    end unless options.blank?
     r
   end
 
-  def parse_toc opts
-    unless opts.blank?
-      r = make_options(opts, [:font_name, :header_text], "toc")
-      r += make_options(opts, [:depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size, :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation, :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation], "toc", :numeric)
-      r + make_options(opts, [:no_dots, :disable_links, :disable_back_links], "toc", :boolean)
+  def parse_toc options
+    unless options.blank?
+      r = make_options(options, [ :font_name, :header_text], "toc")
+      r +=make_options(options, [ :depth,
+                                  :header_fs,
+                                  :l1_font_size,
+                                  :l2_font_size,
+                                  :l3_font_size,
+                                  :l4_font_size,
+                                  :l5_font_size,
+                                  :l6_font_size,
+                                  :l7_font_size,
+                                  :l1_indentation,
+                                  :l2_indentation,
+                                  :l3_indentation,
+                                  :l4_indentation,
+                                  :l5_indentation,
+                                  :l6_indentation,
+                                  :l7_indentation], "toc", :numeric)
+      r +=make_options(options, [ :no_dots,
+                                  :disable_links,
+                                  :disable_back_links], "toc", :boolean)
     end
   end
 
-  def parse_outline opts
-    unless opts.blank?
-      r = make_options(opts, [:outline], "", :boolean)
-      r + make_options(opts, [:outline_depth], "", :numeric)
+  def parse_outline options
+    unless options.blank?
+      r = make_options(options, [:outline], "", :boolean)
+      r +=make_options(options, [:outline_depth], "", :numeric)
     end
   end
 
-  def parse_margins opts
-    make_options(opts, [:top, :bottom, :left, :right], "margin", :numeric) unless opts.blank?
+  def parse_margins options
+    make_options(options, [:top, :bottom, :left, :right], "margin", :numeric) unless options.blank?
   end
 
-  def parse_others opts
-    unless opts.blank?
-      r = make_options(opts, [:orientation, :page_size, :proxy, :username, :password, :cover, :dpi, :encoding, :user_style_sheet])
-      r += make_options(opts, [:redirect_delay, :zoom, :page_offset], "", :numeric)
-      r + make_options(opts, [:book, :default_header, :disable_javascript, :greyscale, :lowquality, :enable_plugins, :disable_internal_links, :disable_external_links, :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background], "", :boolean)
+  def parse_others options
+    unless options.blank?
+      r = make_options(options, [ :orientation,
+                                  :page_size,
+                                  :proxy,
+                                  :username,
+                                  :password,
+                                  :cover,
+                                  :dpi,
+                                  :encoding,
+                                  :user_style_sheet])
+      r +=make_options(options, [ :redirect_delay,
+                                  :zoom,
+                                  :page_offset], "", :numeric)
+      r +=make_options(options, [ :book,
+                                  :default_header,
+                                  :disable_javascript,
+                                  :greyscale,
+                                  :lowquality,
+                                  :enable_plugins,
+                                  :disable_internal_links,
+                                  :disable_external_links,
+                                  :print_media_type,
+                                  :disable_smart_shrinking,
+                                  :use_xserver,
+                                  :no_background], "", :boolean)
     end
   end
 end
--- test/wicked_pdf_test.rb
+++ test/wicked_pdf_test.rb
@@ -53,40 +53,79 @@ class WickedPdfTest < ActiveSupport::TestCase
   test "should parse header and footer options" do
     wp = WickedPdf.new
 
-    [:header, :footer].collect do |hf|
-      [:center, :font_name, :left, :right].collect {|o| assert_equal wp.get_parsed_options(hf => {o => "header_footer"}).strip, "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 'header_footer'"}
-      [:font_size, :spacing].collect {|o| assert_equal wp.get_parsed_options(hf => {o => "12"}).strip, "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 12"}
-      assert_equal wp.get_parsed_options(hf => {:line => true}).strip, "--#{hf.to_s}-line"
-      assert_equal wp.get_parsed_options(hf => {:html => {:url => 'http://www.abc.com'}}).strip, "--#{hf.to_s}-html 'http://www.abc.com'"
+    [:header, :footer].each do |hf|
+      [:center, :font_name, :left, :right].each do |o|
+        assert_equal  "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 'header_footer'",
+                      wp.get_parsed_options(hf => {o => "header_footer"}).strip
+      end
+
+      [:font_size, :spacing].each do |o|
+        assert_equal  "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 12",
+                      wp.get_parsed_options(hf => {o => "12"}).strip
+      end
+
+      assert_equal  "--#{hf.to_s}-line",
+                    wp.get_parsed_options(hf => {:line => true}).strip
+      assert_equal  "--#{hf.to_s}-html 'http://www.abc.com'",
+                    wp.get_parsed_options(hf => {:html => {:url => 'http://www.abc.com'}}).strip
     end
   end
 
   test "should parse toc options" do
     wp = WickedPdf.new
 
-    [:font_name, :header_text].collect {|o| assert_equal wp.get_parsed_options(:toc => {o => "toc"}).strip, "--toc-#{o.to_s.gsub('_', '-')} 'toc'"}
-    [:depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size, :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation, :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation].collect {|o| assert_equal wp.get_parsed_options(:toc => {o => 5}).strip, "--toc-#{o.to_s.gsub('_', '-')} 5"}
-    [:no_dots, :disable_links, :disable_back_links].collect {|o| assert_equal wp.get_parsed_options(:toc => {o => true}).strip, "--toc-#{o.to_s.gsub('_', '-')}"}
+    [:font_name, :header_text].each do |o|
+      assert_equal  "--toc-#{o.to_s.gsub('_', '-')} 'toc'",
+                    wp.get_parsed_options(:toc => {o => "toc"}).strip
+    end
+
+    [ :depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size,
+      :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation,
+      :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation
+    ].each do |o|
+      assert_equal  "--toc-#{o.to_s.gsub('_', '-')} 5",
+                    wp.get_parsed_options(:toc => {o => 5}).strip
+    end
+
+    [:no_dots, :disable_links, :disable_back_links].each do |o|
+      assert_equal  "--toc-#{o.to_s.gsub('_', '-')}",
+                    wp.get_parsed_options(:toc => {o => true}).strip
+    end
   end
 
   test "should parse outline options" do
     wp = WickedPdf.new
 
-    assert_equal wp.get_parsed_options(:outline => {:outline => true}).strip, "--outline"
-    assert_equal wp.get_parsed_options(:outline => {:outline_depth => 5}).strip, "--outline-depth 5"
+    assert_equal "--outline", wp.get_parsed_options(:outline => {:outline => true}).strip
+    assert_equal "--outline-depth 5", wp.get_parsed_options(:outline => {:outline_depth => 5}).strip
   end
 
   test "should parse margins options" do
     wp = WickedPdf.new
 
-    [:top, :bottom, :left, :right].collect {|o| assert_equal wp.get_parsed_options(:margin => {o => "12"}).strip, "--margin-#{o.to_s} 12"}
+    [:top, :bottom, :left, :right].each do |o|
+      assert_equal "--margin-#{o.to_s} 12", wp.get_parsed_options(:margin => {o => "12"}).strip
+    end
   end
 
   test "should parse other options" do
     wp = WickedPdf.new
 
-    [:orientation, :page_size, :proxy, :username, :password, :cover, :dpi, :encoding, :user_style_sheet].collect {|o| assert_equal wp.get_parsed_options(o => "opts").strip, "--#{o.to_s.gsub('_', '-')} 'opts'"}
-    [:redirect_delay, :zoom, :page_offset].collect {|o| assert_equal wp.get_parsed_options(o => 5).strip, "--#{o.to_s.gsub('_', '-')} 5"}
-    [:book, :default_header, :disable_javascript, :greyscale, :lowquality, :enable_plugins, :disable_internal_links, :disable_external_links, :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background].collect {|o| assert_equal wp.get_parsed_options(o => true).strip, "--#{o.to_s.gsub('_', '-')}"}
+    [ :orientation, :page_size, :proxy, :username, :password, :cover, :dpi,
+      :encoding, :user_style_sheet
+    ].each do |o|
+      assert_equal "--#{o.to_s.gsub('_', '-')} 'opts'", wp.get_parsed_options(o => "opts").strip
+    end
+
+    [:redirect_delay, :zoom, :page_offset].each do |o|
+      assert_equal "--#{o.to_s.gsub('_', '-')} 5", wp.get_parsed_options(o => 5).strip
+    end
+
+    [ :book, :default_header, :disable_javascript, :greyscale, :lowquality,
+      :enable_plugins, :disable_internal_links, :disable_external_links,
+      :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background
+    ].each do |o|
+      assert_equal "--#{o.to_s.gsub('_', '-')}", wp.get_parsed_options(o => true).strip
+    end
   end
 end
