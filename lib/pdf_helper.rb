cce696840a43ae6de37c283d4505f73e7c937642
trooster <trooster@interstroom.nl>
2010-07-02 16:10:01 +0200

    option to not send the pdf file (:save_only => true)

--- lib/pdf_helper.rb
+++ lib/pdf_helper.rb
@@ -38,7 +38,7 @@ module PdfHelper
       else
         pdf_content = make_pdf(options)
         File.open(options[:save_to_file], 'wb') {|file| file << pdf_content } if options[:save_to_file]
-        send_data(pdf_content, :filename => pdf_name + '.pdf', :type => 'application/pdf')
+        send_data(pdf_content, :filename => pdf_name + '.pdf', :type => 'application/pdf') unless options[:save_only]
       end
     end
 
