eb844e7f46c0695a593d938b44b1026fc8e295bc
Etienne Samson <tiennou7@gmail.com>
2010-09-02 11:29:01 +0200

    Also fetch stderr, so that the generation error can be reported in the exception.

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -24,16 +24,16 @@ class WickedPdf
   def pdf_from_string(string, options={})
     command_for_stdin_stdout = "#{@exe_path} #{parse_options(options)} -q - - " # -q for no errors on stdout
     p "*"*15 + command_for_stdin_stdout + "*"*15 unless defined?(Rails) and Rails.env != 'development'
-    pdf = begin
+    pdf, err = begin
       Open3.popen3(command_for_stdin_stdout) do |stdin, stdout, stderr|
         stdin.write(string)
         stdin.close
-        stdout.read
+        [stdout.read, stderr.read]
       end
     rescue Exception => e
       raise "Failed to execute #{@exe_path}: #{e}"
     end
-    raise "PDF could not be generated!\n#{stderr.read}" if pdf and pdf.length == 0
+    raise "PDF could not be generated!\n#{err}" if pdf and pdf.length == 0
     pdf
   end
 
