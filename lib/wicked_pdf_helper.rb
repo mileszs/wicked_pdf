1baea9a7cf05116e68c516b823f11675f7519d42
David Jones <unixmonkey1@gmail.com>
2011-01-13 10:59:30 -0500

    Use Rails.root pathname for better cross-OS compatability

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -1,5 +1,5 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(style)
-    stylesheet_link_tag style, "#{Rails.root}/public/stylesheets/#{style}"
+    stylesheet_link_tag style, Rails.root('public','stylesheets',style).to_s
   end
 end
--- test/wicked_pdf_helper_test.rb
+++ test/wicked_pdf_helper_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class WickedPdfHelperTest < ActionView::TestCase
   test 'should return the same as stylesheet_link_tag when passed a full path' do
-    assert_equal wicked_pdf_stylesheet_link_tag("pdf"), stylesheet_link_tag("pdf", "#{Rails.root}/public/stylesheets/pdf")
+    assert_equal wicked_pdf_stylesheet_link_tag('pdf'),
+                 stylesheet_link_tag('pdf', Rails.root.join('public','stylesheets','pdf').to_s)
   end
 end
