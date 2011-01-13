5d5bc2b622e1e0082f20d23c7407da6dca384ea9
David Jones <unixmonkey1@gmail.com>
2011-01-13 16:49:46 -0500

    Rename tests to reflect helper being tested

--- test/wicked_pdf_helper_test.rb
+++ test/wicked_pdf_helper_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class WickedPdfHelperTest < ActionView::TestCase
-  test 'should return the same as stylesheet_link_tag when passed a full path' do
+  test 'wicked_pdf_stylesheet_link_tag should return the same as stylesheet_link_tag when passed a full path' do
     assert_equal wicked_pdf_stylesheet_link_tag('pdf'),
                  stylesheet_link_tag('pdf', Rails.root.join('public','stylesheets','pdf').to_s)
   end
 
-  test 'should return the same as image_tag when passed a full path' do
+  test 'wicked_pdf_image_tag should return the same as image_tag when passed a full path' do
     assert_equal wicked_pdf_image_tag('pdf'),
                  image_tag(Rails.root.join('public','images','pdf').to_s)
   end
 
-  test 'should return the same as javascript_src_tag when passed a full path' do
+  test 'wicked_pdf_javascript_src_tag should return the same as javascript_src_tag when passed a full path' do
     assert_equal wicked_pdf_javascript_src_tag('pdf'),
                  javascript_src_tag(Rails.root.join('public','javascripts','pdf').to_s, {})
   end
 
-  test 'should return many wicked_pdf_javascript_src_tags on wicked_pdf_javascript_include_tag' do
+  test 'wicked_pdf_include_tag should return many wicked_pdf_javascript_src_tags' do
     assert_equal wicked_pdf_javascript_include_tag('foo', 'bar'),
                  [wicked_pdf_javascript_src_tag('foo'), wicked_pdf_javascript_src_tag('bar')].join("\n")
   end
