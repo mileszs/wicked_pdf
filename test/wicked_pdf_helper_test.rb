88515887c39ee09c2f829f30e01077f3b5996bed
David Jones <unixmonkey1@gmail.com>
2011-01-13 10:57:13 -0500

    Better reveal the intent of stylesheet helper test

--- test/wicked_pdf_helper_test.rb
+++ test/wicked_pdf_helper_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class WickedPdfHelperTest < ActionView::TestCase
-  test "should return valid html code stylesheet_link_tag helper" do
+  test 'should return the same as stylesheet_link_tag when passed a full path' do
     assert_equal wicked_pdf_stylesheet_link_tag("pdf"), stylesheet_link_tag("pdf", "#{Rails.root}/public/stylesheets/pdf")
   end
 end
