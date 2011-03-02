e9773be47329a3e04dac9a266c9e4444164eadcd
Roger Campos <roger@itnig.net>
2011-03-02 13:38:04 +0100

    Allowing pdf generation inside basic authenticated domains

--- lib/wicked_pdf_railtie.rb
+++ lib/wicked_pdf_railtie.rb
@@ -6,20 +6,20 @@ if defined?(Rails)
     unless ActionController::Base.instance_methods.include? "render_with_wicked_pdf"
       ActionController::Base.send :include, PdfHelper
     end
-  
+
     unless ActionView::Base.instance_methods.include? "wicked_pdf_stylesheet_link_tag"
       ActionView::Base.send :include, WickedPdfHelper
     end
-  
+
     Mime::Type.register 'application/pdf', :pdf
-  
+
   else
     class WickedRailtie < Rails::Railtie
-  
+
       initializer "wicked_pdf.register" do |app|
         ActionController::Base.send :include, PdfHelper
         ActionView::Base.send :include, WickedPdfHelper
-  
+
         Mime::Type.register 'application/pdf', :pdf
       end
     end
