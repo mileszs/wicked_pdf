f73f052637214ac26e61130b80372f9186026da7
Nathan Stitt <nathan@stitt.org>
2011-04-04 10:58:50 -0500

    in ruby ver 1.9, tempfile passes nil for n if there are no copies of the file present.  Default it to 0 to prevent using nil in later sprintf call

--- lib/wicked_pdf_tempfile.rb
+++ lib/wicked_pdf_tempfile.rb
@@ -2,7 +2,7 @@ require 'tempfile'
 
 class WickedPdfTempfile < Tempfile
   # Replaces Tempfile's +make_tmpname+ with one that honors file extensions.
-  def make_tmpname(basename, n)
+  def make_tmpname(basename, n=0)
     extension = File.extname(basename)
     sprintf("%s_%d_%d%s", File.basename(basename, extension), $$, n, extension)
   end
