b0820781b6fe63eda8836b7b078db96614d688c9
Andrew Coleman <penguincoder@gmail.com>
2011-04-14 15:12:37 -0500

    use binmode with Open3.capture to handle different character sets in ruby1.9 better

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -24,12 +24,8 @@ class WickedPdf
   def pdf_from_string(string, options={})
     command_for_stdin_stdout = "#{@exe_path} #{parse_options(options)} -q - - " # -q for no errors on stdout
     p "*"*15 + command_for_stdin_stdout + "*"*15 unless defined?(Rails) and Rails.env != 'development'
-    pdf, err = begin
-      Open3.popen3(command_for_stdin_stdout) do |stdin, stdout, stderr|
-        stdin.write(string)
-        stdin.close
-        [stdout.read, stderr.read]
-      end
+    pdf, err, exit_status = begin
+      Open3.capture3(command_for_stdin_stdout, :stdin_data => string, :binmode => true)
     rescue Exception => e
       raise "Failed to execute #{@exe_path}: #{e}"
     end
