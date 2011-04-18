788ad4ed8b498099ea65976f7b9f08d701086d03
David Jones <unixmonkey1@gmail.com>
2011-04-18 15:37:55 -0400

    Revert use of capture3, but make binmode for non-ascii charsets

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -25,8 +25,15 @@ class WickedPdf
   def pdf_from_string(string, options={})
     command_for_stdin_stdout = "#{@exe_path} #{parse_options(options)} -q - - " # -q for no errors on stdout
     p "*"*15 + command_for_stdin_stdout + "*"*15 unless defined?(Rails) and Rails.env != 'development'
-    pdf, err, exit_status = begin
-      Open3.capture3(command_for_stdin_stdout, :stdin_data => string, :binmode => true)
+    pdf, err = begin
+      Open3.popen3(command_for_stdin_stdout) do |stdin, stdout, stderr|
+        stdin.binmode
+        stdout.binmode
+        stderr.binmode
+        stdin.write(string)
+        stdin.close
+        [stdout.read, stderr.read]
+      end
     rescue Exception => e
       raise "Failed to execute #{@exe_path}: #{e}"
     end
