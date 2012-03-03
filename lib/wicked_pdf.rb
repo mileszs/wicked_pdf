32c0189f3aee80916b1de0f9f0fb4405ea5fabd7
David Jones <unixmonkey1@gmail.com>
2012-03-03 09:54:48 -0500

    Fix for windows and ruby >= 1.9.2 (from pull req #97 via babinho)

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -4,7 +4,7 @@
 require 'logger'
 require 'digest/md5'
 require 'rbconfig'
-require RbConfig::CONFIG['target_os'] == 'mingw32' ? 'win32/open3' : 'open3'
+require RbConfig::CONFIG['target_os'] == 'mingw32' && !(RUBY_VERSION =~ /1.9/) ? 'win32/open3' : 'open3'
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/core_ext/object/blank'
 
