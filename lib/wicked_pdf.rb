b2c2acea82d973e93343323e2a04a6e4e4aa8fb2
Anton Averin <a.a.averin@gmail.com>
2012-04-03 10:10:33 +0400

    Latest wkhtmltopdf versions are very picky about arguments place. All global (margin, -q, others) arguments should go before header/footer/toc arguments. Also, ToC is generated not by '--toc', but by 'toc' argument.

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -25,7 +25,7 @@ class WickedPdf
   end
 
   def pdf_from_string(string, options={})
-    command = "\"#{@exe_path}\" #{parse_options(options)} #{'-q ' unless on_windows?}- - " # -q for no errors on stdout
+    command = "\"#{@exe_path}\" #{'-q ' unless on_windows?} #{parse_options(options)} - - " # -q for no errors on stdout
     print_command(command) if in_development_mode?
     pdf, err = Open3.popen3(command) do |stdin, stdout, stderr|
       stdin.binmode
@@ -58,13 +58,13 @@ class WickedPdf
 
     def parse_options(options)
       [
+        parse_margins(options.delete(:margin)),
+        parse_others(options),
         parse_header_footer(:header => options.delete(:header),
                             :footer => options.delete(:footer),
                             :layout => options[:layout]),
         parse_toc(options.delete(:toc)),
         parse_outline(options.delete(:outline)),
-        parse_margins(options.delete(:margin)),
-        parse_others(options),
         parse_basic_auth(options)
       ].join(' ')
     end
@@ -111,7 +111,7 @@ class WickedPdf
     end
 
     def parse_toc(options)
-      r = '--toc ' unless options.nil?
+      r = 'toc ' unless options.nil?
       unless options.blank?
         r += make_options(options, [ :font_name, :header_text], "toc")
         r +=make_options(options, [ :depth,
