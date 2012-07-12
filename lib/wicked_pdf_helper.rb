099b5820bb1f343248f8d6b8aec5b1c941a36251
Tom Brooks <d3vkit@rs>
2012-07-12 15:22:17 -0700

    Fixing helpers to not fail in rails < 2.3.5

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -1,21 +1,39 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
-    css_dir = Rails.root.join('public','stylesheets')
-    sources.collect { |source|
-      "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
-    }.join("\n").html_safe
+    
+    if Rails.root.kind_of?(Pathname)
+      root_pathname = Rails.root
+    elsif Rails.root.kind_of?(String)
+      root_obj = Pathname(Rails.root)
+    end
+
+    css_dir = root_obj.join('public','stylesheets')
+    
+    begin
+      sources.collect { |source|
+        "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
+      }.join("\n").html_safe
+    rescue NoMethodError
+      sources.collect { |source|
+        "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
+      }.join("\n")
+    end
   end
 
   def wicked_pdf_image_tag(img, options={})
-    image_tag "file:///#{Rails.root.join('public', 'images', img)}", options
+    image_tag "file:///#{root_obj.join('public', 'images', img)}", options
   end
 
   def wicked_pdf_javascript_src_tag(jsfile, options={})
-    javascript_src_tag "file:///#{Rails.root.join('public','javascripts',jsfile)}", options
+    javascript_src_tag "file:///#{root_obj.join('public','javascripts',jsfile)}", options
   end
 
   def wicked_pdf_javascript_include_tag(*sources)
-    sources.collect{ |source| wicked_pdf_javascript_src_tag(source, {}) }.join("\n").html_safe
+    begin
+      sources.collect{ |source| wicked_pdf_javascript_src_tag(source, {}) }.join("\n").html_safe
+    rescue NoMethodError
+      sources.collect{ |source| wicked_pdf_javascript_src_tag(source, {}) }.join("\n")
+    end
   end
 
   module Assets
