35e1a99c6a149d147883613dfcd9fb5a47c2f028
Matt <matt@zurb.com>
2012-10-29 14:26:26 -0700

    Fix performance issue for documents with very long lines by interfacing with wkhtmltopdf using files instead of stdin/stdout.

--- test/wicked_pdf_test.rb
+++ test/wicked_pdf_test.rb
@@ -19,6 +19,15 @@ class WickedPdfTest < ActiveSupport::TestCase
     assert pdf.rstrip.end_with?("%%EOF")
     assert pdf.length > 100
   end
+  
+  test "should generate PDF from html document with long lines" do
+    wp = WickedPdf.new
+    document_with_long_line_file = File.new("test/fixtures/document_with_long_line.html", "r")
+    pdf = wp.pdf_from_string(document_with_long_line_file.read)
+    assert pdf.start_with?("%PDF-1.4")
+    assert pdf.rstrip.end_with?("%%EOF")
+    assert pdf.length > 100
+  end
 
   test "should raise exception when no path to wkhtmltopdf" do
     assert_raise RuntimeError do
