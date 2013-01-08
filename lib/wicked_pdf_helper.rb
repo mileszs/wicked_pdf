94fe9c1ec5f1fdcc6a5e89e3f9baf36a987ecb5a
David Jones <unixmonkey1@gmail.com>
2013-01-08 12:45:27 -0500

    Add file extensions where necessary to assets

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -3,10 +3,15 @@ module WickedPdfHelper
     String === Rails.root ? Pathname.new(Rails.root) : Rails.root
   end
 
+  def self.add_extension(filename, extension)
+    (File.extname(filename.to_s)[1..-1] == extension) ? filename : "#{filename}.#{extension}"
+  end
+
   def wicked_pdf_stylesheet_link_tag(*sources)
     css_dir = WickedPdfHelper.root_path.join('public', 'stylesheets')
     css_text = sources.collect { |source|
-      "<style type='text/css'>#{File.read(css_dir.join(source+'.css'))}</style>"
+      source = WickedPdfHelper.add_extension(source, 'css')
+      "<style type='text/css'>#{File.read(css_dir.join(source))}</style>"
     }.join("\n")
     css_text.respond_to?(:html_safe) ? css_text.html_safe : css_text
   end
@@ -16,6 +21,7 @@ module WickedPdfHelper
   end
 
   def wicked_pdf_javascript_src_tag(jsfile, options={})
+    jsfile = WickedPdfHelper.add_extension(jsfile, 'js')
     javascript_src_tag "file:///#{WickedPdfHelper.root_path.join('public', 'javascripts', jsfile)}", options
   end
 
@@ -27,7 +33,8 @@ module WickedPdfHelper
   module Assets
     def wicked_pdf_stylesheet_link_tag(*sources)
       sources.collect { |source|
-        "<style type='text/css'>#{read_asset(source+".css")}</style>"
+        source = WickedPdfHelper.add_extension(source, 'css')
+        "<style type='text/css'>#{read_asset(source)}</style>"
       }.join("\n").html_safe
     end
 
@@ -36,12 +43,14 @@ module WickedPdfHelper
     end
 
     def wicked_pdf_javascript_src_tag(jsfile, options={})
+      jsfile = WickedPdfHelper.add_extension(jsfile, 'js')
       javascript_include_tag "file:///#{asset_pathname(jsfile).to_s}", options
     end
 
     def wicked_pdf_javascript_include_tag(*sources)
       sources.collect { |source|
-        "<script type='text/javascript'>#{read_asset(source+".js")}</script>"
+        source = WickedPdfHelper.add_extension(source, 'js')
+        "<script type='text/javascript'>#{read_asset(source)}</script>"
       }.join("\n").html_safe
     end
 
