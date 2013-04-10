16febd178f73ce0815f3f655b26c9f48ad606ae9
sharpyfox <sharpyfox@gmail.com>
2013-04-10 23:08:34 +0400

    real 0.11.0 version info

--- test/unit/wicked_pdf_test.rb
+++ test/unit/wicked_pdf_test.rb
@@ -163,8 +163,8 @@ class WickedPdfTest < ActiveSupport::TestCase
   end
 
   test "should extract new wkhtmltopdf version" do
-    version_info_sample = "Name:\n  wkhtmltopdf 0.11.2\n\nLicense:\n  Copyright (C) 2008,2009 Wkhtmltopdf Authors.\n\n\n\n  License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n  This is free software: you are free to change and redistribute it. There is NO\n  WARRANTY, to the extent permitted by law.\n\nAuthors:\n  Written by Jakob Truelsen. Patches by Mrio Silva, Benoit Garret and Emmanuel\n  Bouthenot.\n"
-    assert_equal Gem::Version.new('0.11.2'), @wp.send(:parse_version, version_info_sample)
+    version_info_sample = "Name:\n  wkhtmltopdf 0.11.0 rc2\n\nLicense:\n  Copyright (C) 2010 wkhtmltopdf/wkhtmltoimage Authors.\n\n\n\n  License LGPLv3+: GNU Lesser General Public License version 3 or later\n  <http://gnu.org/licenses/lgpl.html>. This is free software: you are free to\n  change and redistribute it. There is NO WARRANTY, to the extent permitted by\n  law.\n\nAuthors:\n  Written by Jan Habermann, Christian Sciberras and Jakob Truelsen. Patches by\n  Mehdi Abbad, Lyes Amazouz, Pascal Bach, Emmanuel Bouthenot, Benoit Garret and\n  Mario Silva."
+    assert_equal Gem::Version.new('0.11.0'), @wp.send(:parse_version, version_info_sample)
   end
 
   test "should extract wkhtmltopdf version with nondigit symbols" do
