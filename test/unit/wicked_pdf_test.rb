96793ad1ec6bd4d6a17c606c59d91be19b7dc6e7
David Jones <unixmonkey1@gmail.com>
2014-08-17 10:21:50 -0400

    Single space before first argument

--- test/unit/wicked_pdf_test.rb
+++ test/unit/wicked_pdf_test.rb
@@ -95,19 +95,19 @@ class WickedPdfTest < ActiveSupport::TestCase
 
     [:header, :footer].each do |hf|
       [:center, :font_name, :left, :right].each do |o|
-        assert_equal  "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} header_footer",
-                      wp.get_parsed_options(hf => {o => "header_footer"}).strip
+        assert_equal "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} header_footer",
+                     wp.get_parsed_options(hf => {o => "header_footer"}).strip
       end
 
       [:font_size, :spacing].each do |o|
-        assert_equal  "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 12",
-                      wp.get_parsed_options(hf => {o => "12"}).strip
+        assert_equal "--#{hf.to_s}-#{o.to_s.gsub('_', '-')} 12",
+                     wp.get_parsed_options(hf => {o => "12"}).strip
       end
 
-      assert_equal  "--#{hf.to_s}-line",
-                    wp.get_parsed_options(hf => {:line => true}).strip
-      assert_equal  "--#{hf.to_s}-html http://www.abc.com",
-                    wp.get_parsed_options(hf => {:html => {:url => 'http://www.abc.com'}}).strip
+      assert_equal "--#{hf.to_s}-line",
+                   wp.get_parsed_options(hf => {:line => true}).strip
+      assert_equal "--#{hf.to_s}-html http://www.abc.com",
+                   wp.get_parsed_options(hf => {:html => {:url => 'http://www.abc.com'}}).strip
     end
   end
 
@@ -116,21 +116,21 @@ class WickedPdfTest < ActiveSupport::TestCase
     toc_option = wp.get_valid_option('toc')
 
     [:font_name, :header_text].each do |o|
-      assert_equal  "#{toc_option} --toc-#{o.to_s.gsub('_', '-')} toc",
-                    wp.get_parsed_options(:toc => {o => "toc"}).strip
+      assert_equal "#{toc_option} --toc-#{o.to_s.gsub('_', '-')} toc",
+                   wp.get_parsed_options(:toc => {o => "toc"}).strip
     end
 
     [ :depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size,
       :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation,
       :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation
     ].each do |o|
-      assert_equal  "#{toc_option} --toc-#{o.to_s.gsub('_', '-')} 5",
-                    wp.get_parsed_options(:toc => {o => 5}).strip
+      assert_equal "#{toc_option} --toc-#{o.to_s.gsub('_', '-')} 5",
+                   wp.get_parsed_options(:toc => {o => 5}).strip
     end
 
     [:no_dots, :disable_links, :disable_back_links].each do |o|
-      assert_equal  "#{toc_option} --toc-#{o.to_s.gsub('_', '-')}",
-                    wp.get_parsed_options(:toc => {o => true}).strip
+      assert_equal "#{toc_option} --toc-#{o.to_s.gsub('_', '-')}",
+                   wp.get_parsed_options(:toc => {o => true}).strip
     end
   end
 
