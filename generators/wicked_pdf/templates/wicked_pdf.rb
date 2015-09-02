16b87f3442bda352df60252342492bf7676621c4
David Jones <unixmonkey1@gmail.com>
2015-09-02 09:03:09 -0400

    Comment out exe_path in generator by default
    This typically isn't needed when used with the recommended
    wkhtmltopdf-binary gems, and WickedPDF tries to guess when it isn't
    specified.

--- generators/wicked_pdf/templates/wicked_pdf.rb
+++ generators/wicked_pdf/templates/wicked_pdf.rb
@@ -1,5 +1,21 @@
+# WickedPDF Global Configuration
+#
+# Use this to set up shared configuration options for your entire application.
+# Any of the configuration options shown here can also be applied to single
+# models by passing arguments to the `render :pdf` call.
+#
+# To learn more, check out the README:
+#
+# https://github.com/mileszs/wicked_pdf/blob/master/README.md
+
 WickedPdf.config = {
-  #wkhtmltopdf: '/usr/local/bin/wkhtmltopdf',
-  #layout: "pdf.html",
-  exe_path: '/usr/local/bin/wkhtmltopdf'
+  # Path to the wkhtmltopdf executable: This usually isn't needed if using
+  # one of the wkhtmltopdf-binary family of gems.
+  # exe_path: '/usr/local/bin/wkhtmltopdf',
+  #   or
+  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+
+  # Layout file to be used for all PDFs
+  # (but can be overridden in `render :pdf` calls)
+  # layout: 'pdf.html',
 }
