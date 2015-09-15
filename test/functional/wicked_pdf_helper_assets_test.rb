8e364dfbdf4ffdb25512bc8d92d87a0633eac15c
Ben Sedat <bsedat@tinfoilsecurity.com>
2015-09-15 15:19:19 -0700

    Allow WickedPdfHelperAssetsTest to run for Rails 3.1+

--- test/functional/wicked_pdf_helper_assets_test.rb
+++ test/functional/wicked_pdf_helper_assets_test.rb
@@ -5,7 +5,7 @@ class WickedPdfHelperAssetsTest < ActionView::TestCase
 
   include WickedPdfHelper::Assets
 
-  if Rails::VERSION::MAJOR == 4
+  if Rails::VERSION::MAJOR > 3 || (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR > 0)
     test 'wicked_pdf_asset_path should return a url when assets are served by an asset server' do
       expects(:asset_pathname => 'http://assets.domain.com/dummy.png')
       assert_equal 'http://assets.domain.com/dummy.png', wicked_pdf_asset_path('dummy.png')
