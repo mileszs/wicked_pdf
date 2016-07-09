631f1d07f758bbdb58bd60006aad9274265bc87c
Christian Georgii <cgeorgii@gmail.com>
2016-07-09 12:18:10 +0200

    change after_filter to after_action to eliminate deprecation warning

--- lib/wicked_pdf/pdf_helper.rb
+++ lib/wicked_pdf/pdf_helper.rb
@@ -8,7 +8,7 @@ class WickedPdf
       base.class_eval do
         alias_method_chain :render, :wicked_pdf
         alias_method_chain :render_to_string, :wicked_pdf
-        after_filter :clean_temp_files
+        after_action :clean_temp_files
       end
     end
 
