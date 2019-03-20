91a9a1faa8abb3d6a220d0faa5950ff188b27256
Adam Meehan <adam.meehan@gmail.com>
2019-03-20 18:11:22 +1100

    Encapsulate option parsing
    Moves option parsing methods out of main class to improve clarity
    Cleanups up some private method testing and hackery in main class tests

---
+++ lib/wicked_pdf/option_parser.rb
@@ -0,0 +1,220 @@
+class WickedPdf
+  class OptionParser
+    BINARY_VERSION_WITHOUT_DASHES = Gem::Version.new('0.12.0')
+
+    attr_reader :binary_version
+
+    def initialize(binary_version = WickedPdf::DEFAULT_BINARY_VERSION)
+      @binary_version = binary_version
+    end
+
+    def parse(options)
+      [
+        parse_extra(options),
+        parse_others(options),
+        parse_global(options),
+        parse_outline(options.delete(:outline)),
+        parse_header_footer(:header => options.delete(:header),
+                            :footer => options.delete(:footer),
+                            :layout => options[:layout]),
+        parse_cover(options.delete(:cover)),
+        parse_toc(options.delete(:toc)),
+        parse_basic_auth(options)
+      ].flatten
+    end
+
+    def valid_option(name)
+      if binary_version < BINARY_VERSION_WITHOUT_DASHES
+        "--#{name}"
+      else
+        name
+      end
+    end
+
+    private
+
+    def parse_extra(options)
+      return [] if options[:extra].nil?
+      return options[:extra].split if options[:extra].respond_to?(:split)
+      options[:extra]
+    end
+
+    def parse_basic_auth(options)
+      if options[:basic_auth]
+        user, passwd = Base64.decode64(options[:basic_auth]).split(':')
+        ['--username', user, '--password', passwd]
+      else
+        []
+      end
+    end
+
+    def parse_header_footer(options)
+      r = []
+      unless options.blank?
+        [:header, :footer].collect do |hf|
+          next if options[hf].blank?
+          opt_hf = options[hf]
+          r += make_options(opt_hf, [:center, :font_name, :left, :right], hf.to_s)
+          r += make_options(opt_hf, [:font_size, :spacing], hf.to_s, :numeric)
+          r += make_options(opt_hf, [:line], hf.to_s, :boolean)
+          if options[hf] && options[hf][:content]
+            @hf_tempfiles = [] unless defined?(@hf_tempfiles)
+            @hf_tempfiles.push(tf = WickedPdfTempfile.new("wicked_#{hf}_pdf.html"))
+            tf.write options[hf][:content]
+            tf.flush
+            options[hf][:html] = {}
+            options[hf][:html][:url] = "file:///#{tf.path}"
+          end
+          unless opt_hf[:html].blank?
+            r += make_option("#{hf}-html", opt_hf[:html][:url]) unless opt_hf[:html][:url].blank?
+          end
+        end
+      end
+      r
+    end
+
+    def parse_cover(argument)
+      arg = argument.to_s
+      return [] if arg.blank?
+      # Filesystem path or URL - hand off to wkhtmltopdf
+      if argument.is_a?(Pathname) || (arg[0, 4] == 'http')
+        [valid_option('cover'), arg]
+      else # HTML content
+        @hf_tempfiles ||= []
+        @hf_tempfiles << tf = WickedPdfTempfile.new('wicked_cover_pdf.html')
+        tf.write arg
+        tf.flush
+        [valid_option('cover'), tf.path]
+      end
+    end
+
+    def parse_toc(options)
+      return [] if options.nil?
+      r = [valid_option('toc')]
+      unless options.blank?
+        r += make_options(options, [:font_name, :header_text], 'toc')
+        r += make_options(options, [:xsl_style_sheet])
+        r += make_options(options, [:depth,
+                                    :header_fs,
+                                    :text_size_shrink,
+                                    :l1_font_size,
+                                    :l2_font_size,
+                                    :l3_font_size,
+                                    :l4_font_size,
+                                    :l5_font_size,
+                                    :l6_font_size,
+                                    :l7_font_size,
+                                    :level_indentation,
+                                    :l1_indentation,
+                                    :l2_indentation,
+                                    :l3_indentation,
+                                    :l4_indentation,
+                                    :l5_indentation,
+                                    :l6_indentation,
+                                    :l7_indentation], 'toc', :numeric)
+        r += make_options(options, [:no_dots,
+                                    :disable_links,
+                                    :disable_back_links], 'toc', :boolean)
+        r += make_options(options, [:disable_dotted_lines,
+                                    :disable_toc_links], nil, :boolean)
+      end
+      r
+    end
+
+    def parse_outline(options)
+      r = []
+      unless options.blank?
+        r = make_options(options, [:outline], '', :boolean)
+        r += make_options(options, [:outline_depth], '', :numeric)
+      end
+      r
+    end
+
+    def parse_margins(options)
+      make_options(options, [:top, :bottom, :left, :right], 'margin', :numeric)
+    end
+
+    def parse_global(options)
+      r = []
+      unless options.blank?
+        r += make_options(options, [:orientation,
+                                    :dpi,
+                                    :page_size,
+                                    :page_width,
+                                    :title])
+        r += make_options(options, [:lowquality,
+                                    :grayscale,
+                                    :no_pdf_compression], '', :boolean)
+        r += make_options(options, [:image_dpi,
+                                    :image_quality,
+                                    :page_height], '', :numeric)
+        r += parse_margins(options.delete(:margin))
+      end
+      r
+    end
+
+    def parse_others(options)
+      r = []
+      unless options.blank?
+        r += make_options(options, [:proxy,
+                                    :username,
+                                    :password,
+                                    :encoding,
+                                    :user_style_sheet,
+                                    :viewport_size,
+                                    :window_status])
+        r += make_options(options, [:cookie,
+                                    :post], '', :name_value)
+        r += make_options(options, [:redirect_delay,
+                                    :zoom,
+                                    :page_offset,
+                                    :javascript_delay], '', :numeric)
+        r += make_options(options, [:book,
+                                    :default_header,
+                                    :disable_javascript,
+                                    :enable_plugins,
+                                    :disable_internal_links,
+                                    :disable_external_links,
+                                    :print_media_type,
+                                    :disable_smart_shrinking,
+                                    :use_xserver,
+                                    :no_background,
+                                    :images,
+                                    :no_images,
+                                    :no_stop_slow_scripts], '', :boolean)
+      end
+      r
+    end
+
+    def make_options(options, names, prefix = '', type = :string)
+      return [] if options.nil?
+      names.collect do |o|
+        if options[o].blank?
+          []
+        else
+          make_option("#{prefix.blank? ? '' : prefix + '-'}#{o}",
+                      options[o],
+                      type)
+        end
+      end
+    end
+
+    def make_option(name, value, type = :string)
+      if value.is_a?(Array)
+        return value.collect { |v| make_option(name, v, type) }
+      end
+      if type == :name_value
+        parts = value.to_s.split(' ')
+        ["--#{name.tr('_', '-')}", *parts]
+      elsif type == :boolean
+        if value
+          ["--#{name.tr('_', '-')}"]
+        else
+          []
+        end
+      else
+        ["--#{name.tr('_', '-')}", value.to_s]
+      end
+    end
+  end
+end
--- test/unit/wicked_pdf_test.rb
+++ test/unit/wicked_pdf_test.rb
@@ -2,23 +2,6 @@ require 'test_helper'
 WickedPdf.config = { :exe_path => ENV['WKHTMLTOPDF_BIN'] || '/usr/local/bin/wkhtmltopdf' }
 HTML_DOCUMENT = '<html><body>Hello World</body></html>'.freeze
 
-# Provide a public accessor to the normally-private parse_options function.
-# Also, smash the returned array of options into a single string for
-# convenience in testing below.
-class WickedPdf
-  undef :binary_version
-  undef :binary_version=
-  attr_accessor :binary_version
-
-  def get_parsed_options(opts)
-    parse_options(opts).join(' ')
-  end
-
-  def get_valid_option(name)
-    valid_option(name)
-  end
-end
-
 class WickedPdfTest < ActiveSupport::TestCase
   def setup
     @wp = WickedPdf.new
@@ -86,101 +69,6 @@ class WickedPdfTest < ActiveSupport::TestCase
     end
   end
 
-  test 'should parse header and footer options' do
-    [:header, :footer].each do |hf|
-      [:center, :font_name, :left, :right].each do |o|
-        assert_equal "--#{hf}-#{o.to_s.tr('_', '-')} header_footer",
-                     @wp.get_parsed_options(hf => { o => 'header_footer' }).strip
-      end
-
-      [:font_size, :spacing].each do |o|
-        assert_equal "--#{hf}-#{o.to_s.tr('_', '-')} 12",
-                     @wp.get_parsed_options(hf => { o => '12' }).strip
-      end
-
-      assert_equal "--#{hf}-line",
-                   @wp.get_parsed_options(hf => { :line => true }).strip
-      assert_equal "--#{hf}-html http://www.abc.com",
-                   @wp.get_parsed_options(hf => { :html => { :url => 'http://www.abc.com' } }).strip
-    end
-  end
-
-  test 'should parse toc options' do
-    toc_option = @wp.get_valid_option('toc')
-
-    [:font_name, :header_text].each do |o|
-      assert_equal "#{toc_option} --toc-#{o.to_s.tr('_', '-')} toc",
-                   @wp.get_parsed_options(:toc => { o => 'toc' }).strip
-    end
-
-    [
-      :depth, :header_fs, :l1_font_size, :l2_font_size, :l3_font_size, :l4_font_size,
-      :l5_font_size, :l6_font_size, :l7_font_size, :l1_indentation, :l2_indentation,
-      :l3_indentation, :l4_indentation, :l5_indentation, :l6_indentation, :l7_indentation
-    ].each do |o|
-      assert_equal "#{toc_option} --toc-#{o.to_s.tr('_', '-')} 5",
-                   @wp.get_parsed_options(:toc => { o => 5 }).strip
-    end
-
-    [:no_dots, :disable_links, :disable_back_links].each do |o|
-      assert_equal "#{toc_option} --toc-#{o.to_s.tr('_', '-')}",
-                   @wp.get_parsed_options(:toc => { o => true }).strip
-    end
-  end
-
-  test 'should parse outline options' do
-    assert_equal '--outline', @wp.get_parsed_options(:outline => { :outline => true }).strip
-    assert_equal '--outline-depth 5', @wp.get_parsed_options(:outline => { :outline_depth => 5 }).strip
-  end
-
-  test 'should parse no_images option' do
-    assert_equal '--no-images', @wp.get_parsed_options(:no_images => true).strip
-    assert_equal '--images', @wp.get_parsed_options(:images => true).strip
-  end
-
-  test 'should parse margins options' do
-    [:top, :bottom, :left, :right].each do |o|
-      assert_equal "--margin-#{o} 12", @wp.get_parsed_options(:margin => { o => '12' }).strip
-    end
-  end
-
-  test 'should parse cover' do
-    cover_option = @wp.get_valid_option('cover')
-
-    pathname = Rails.root.join('app', 'views', 'pdf', 'file.html')
-    assert_equal "#{cover_option} http://example.org", @wp.get_parsed_options(:cover => 'http://example.org').strip, 'URL'
-    assert_equal "#{cover_option} #{pathname}", @wp.get_parsed_options(:cover => pathname).strip, 'Pathname'
-    assert_match %r{#{cover_option} .+wicked_cover_pdf.+\.html}, @wp.get_parsed_options(:cover => '<html><body>HELLO</body></html>').strip, 'HTML'
-  end
-
-  test 'should parse other options' do
-    [
-      :orientation, :page_size, :proxy, :username, :password, :dpi,
-      :encoding, :user_style_sheet
-    ].each do |o|
-      assert_equal "--#{o.to_s.tr('_', '-')} opts", @wp.get_parsed_options(o => 'opts').strip
-    end
-
-    [:cookie, :post].each do |o|
-      assert_equal "--#{o.to_s.tr('_', '-')} name value", @wp.get_parsed_options(o => 'name value').strip
-
-      nv_formatter = proc { |number| "--#{o.to_s.tr('_', '-')} par#{number} val#{number}" }
-      assert_equal "#{nv_formatter.call(1)} #{nv_formatter.call(2)}", @wp.get_parsed_options(o => ['par1 val1', 'par2 val2']).strip
-    end
-
-    [:redirect_delay, :zoom, :page_offset].each do |o|
-      assert_equal "--#{o.to_s.tr('_', '-')} 5", @wp.get_parsed_options(o => 5).strip
-    end
-
-    [
-      :book, :default_header, :disable_javascript, :grayscale, :lowquality,
-      :enable_plugins, :disable_internal_links, :disable_external_links,
-      :print_media_type, :disable_smart_shrinking, :use_xserver, :no_background
-    ].each do |o|
-      assert_equal "--#{o.to_s.tr('_', '-')}", @wp.get_parsed_options(o => true).strip
-    end
-  end
-
   test 'should extract old wkhtmltopdf version' do
     version_info_sample = "Name:\n  wkhtmltopdf 0.9.9\n\nLicense:\n  Copyright (C) 2008,2009 Wkhtmltopdf Authors.\n\n\n\n  License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n  This is free software: you are free to change and redistribute it. There is NO\n  WARRANTY, to the extent permitted by law.\n\nAuthors:\n  Written by Jakob Truelsen. Patches by Mrio Silva, Benoit Garret and Emmanuel\n  Bouthenot.\n"
     assert_equal WickedPdf::DEFAULT_BINARY_VERSION, @wp.send(:parse_version, version_info_sample)
@@ -202,28 +90,6 @@ class WickedPdfTest < ActiveSupport::TestCase
 
   test 'should set version on initialize' do
     assert_not_equal @wp.send(:binary_version), ''
-  end
-
-  test 'should not use double dash options for version without dashes' do
-    @wp.binary_version = WickedPdf::BINARY_VERSION_WITHOUT_DASHES
-
-    %w[toc cover].each do |name|
-      assert_equal @wp.get_valid_option(name), name
-    end
-  end
-
-  test 'should use double dash options for version with dashes' do
-    @wp.binary_version = Gem::Version.new('0.11.0')
-
-    %w[toc cover].each do |name|
-      assert_equal @wp.get_valid_option(name), "--#{name}"
-    end
-  end
-
-  test '-- options should not be given after object' do
-    options = { :header => { :center => 3 }, :cover => 'http://example.org', :disable_javascript => true }
-    cover_option = @wp.get_valid_option('cover')
-    assert_equal @wp.get_parsed_options(options), "--disable-javascript --header-center 3 #{cover_option} http://example.org"
   end
 
   test 'should output progress when creating pdfs on compatible hosts' do
